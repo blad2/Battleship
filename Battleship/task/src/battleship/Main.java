74d057a303ad006b00f153e718a8a6001878b4d9
Bladimir Moracher <blad2@users.noreply.github.com>
2022-01-31 00:19:50 -0500

    fixing the name of a ship
    Criuser -> Cruiser

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -10,7 +10,7 @@ public class Main {
         AIRCRAFT_CARRIER("Aircraft Carrier", 5),
         BATTLESHIP("Battleship", 4),
         SUBMARINE("Submarine", 3),
-        CRUISER("Criuser", 3),
+        CRUISER("Cruiser", 3),
         DESTROYER("Destroyer", 2);
 
         final String name;
