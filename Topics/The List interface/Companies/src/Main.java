61c637575c9bcbcc713ec3a0aa6b5e6778a95fbf
blad2 <bladimoracher@gmail.com>
2022-03-21 23:51:43 -0400

    adding topics codes

---
+++ Topics/The List interface/Companies/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Arrays.toString(scanner.nextLine().split(" ")));
+    }
+}
